0f1f35c59e8a41d1062742f971d5f4e6f9afe6ae
Shrisby Sam <samuelahouanvoeke@gmail.com>
2025-07-07 16:29:48 +0100

    save

--- app/Http/Controllers/Api/ModuleController.php
+++ app/Http/Controllers/Api/ModuleController.php
@@ -105,7 +105,7 @@ class ModuleController extends Controller
                 if($detail_subModule['stat'] == 100){
                     Badge::firstOrCreate([
                         'learner_id' => $user->id, 
-                        'module_id' => $moduleStep->id,
+                        'module_id' => $competence->id,
                         'list_badge_id' => $moduleStep->id, 
                         'awarded_at' => new \DateTime(), 
                         'validation_instructor_id' => auth()->user()->id, 
